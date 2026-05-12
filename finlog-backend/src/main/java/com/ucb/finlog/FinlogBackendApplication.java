4c25cadfe1a8efb5fa434d8543c3e0fb77de0814
RafaPrattes <rafaprates92@gmail.com>
2026-05-11 21:46:48 -0300

    Inicializando projeto base do Spring Boot

---
+++ finlog-backend/src/main/java/com/ucb/finlog/FinlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.finlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinlogBackendApplication.class, args);
+	}
+
+}
---
+++ finlog-backend/src/test/java/com/ucb/finlog/FinlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucb.finlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
